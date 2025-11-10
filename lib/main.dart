49911b2ca1e6afe603913eb2d0676b60007be4f7
Amir <amirdorri81@gmail.com>
2025-11-10 21:01:34 +0330

    changing theme is working fine

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:hotelino/core/theme/app_theme.dart';
 import 'package:hotelino/core/theme/theme_provider.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-  final widgetBinding = WidgetsFlutterBinding.ensureInitialized();
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (_) => ThemeProvider(
-            WidgetsBinding.instance.platformDispatcher.platformBrightness,
+  final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider(
+          create: (_) => ThemeProvider(WidgetsBinding.instance.platformDispatcher.platformBrightness)
           ),
-        ),
-      ],
-      child: const MyApp(),
-    ),
-  );
+    ],
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {}
+  Widget build(BuildContext context) {
+    return Consumer<ThemeProvider>(
+      builder: (context, themeModeProvider, child) {
+        return MaterialApp(
+          theme: themeModeProvider.brightness == Brightness.light ? AppTheme.lightTheme : AppTheme.darkTheme,
+          home: Scaffold(
+            appBar: AppBar(),
+            body: Center(
+              child: ElevatedButton(
+                  onPressed: () {
+                    themeModeProvider.toggleTheme();
+                  },
+                  child: Text('change theme')),
+            ),
+          ),
+        );
+      },
+    );
+  }
 }
+
+
