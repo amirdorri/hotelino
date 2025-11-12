abda3f7aaa8388e10c5468c4573dc6cc26583ceb
Amir <amirdorri81@gmail.com>
2025-11-12 10:25:34 +0330

    onboarding provider added

---
+++ lib/features/onBoarding/presentation/onboarding_provider.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:hotelino/features/onboarding/repository/onboarding_repo.dart';
+
+class OnboardingProvider extends ChangeNotifier {
+    final OnboardingRepo repository;
+    OnboardingProvider(this.repository){
+        _loadData();
+    }
+    int currentPage;
+    List<Map<String, String>> onboardingData = [];
+
+    void _loadData(){
+        onboardingData = repository.onboardingData;
+        notifyListeners();
+    }
+    void updateCurrentPage(int index){
+        currentPage = index;
+        notifyListeners();
+    }
+}
