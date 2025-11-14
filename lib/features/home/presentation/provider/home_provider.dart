be8450134bf71a7eb3a3997b14c0c1b340c2aa24
Amir <amirdorri81@gmail.com>
2025-11-14 18:07:36 +0330

    add home_provider

---
+++ lib/features/home/presentation/provider/home_provider.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:hotelino/features/home/data/hotel_repository.dart';
+import 'package:hotelino/model/HotelModel.dart';
+
+class HomeProvider extends ChangeNotifier {
+  final HotelRepository _hotelRepository;
+
+  HomeProvider(this._hotelRepository) {
+    fetchHotels();
+  }
+
+  List<HotelModel> _hotels = [];
+  List<HotelModel> get hotels => _hotels;
+  
+  fetchHotels() async {
+    _hotels = await _hotelRepository.fetchHotels();
+    notifyListeners();
+  }
+}
