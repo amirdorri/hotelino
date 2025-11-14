c3ece4e19176ac7b0098e8e6dcd042371bd469bf
Amir <amirdorri81@gmail.com>
2025-11-14 17:55:29 +0330

    add hotel_repository.dart

---
+++ lib/features/home/data/hotel_repository.dart
@@ -0,0 +1,20 @@
+import 'package:hotelino/model/HotelModel.dart';
+import 'package:hotelino/shared/service/json_data_service.dart';
+
+class HotelRepository {
+  final JsonDataService jsonDataService;
+
+  HotelRepository({required this.jsonDataService});
+
+  Future<List<HotelModel>> fetchHotels() async {
+    return jsonDataService.loadHotels();
+  }
+
+  Future<HotelModel> getHotelById(String id) {
+    return jsonDataService.loadHotels().then(
+            (hotels) {
+          return hotels.firstWhere((element) => element.id == id);
+        }
+    );
+  }
+}
