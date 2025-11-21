f3baeacc0538b40091b41a0ba3ba27fe75cb464d
Amir <amirdorri81@gmail.com>
2025-11-21 11:04:06 +0330

    price_formatter.dart added and hotel_card.dart get started

--- lib/features/home/presentation/provider/widgets/hotel_card.dart
+++ lib/features/home/presentation/provider/widgets/hotel_card.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:hotelino/features/home/presentation/provider/favorite_item_provider.dart';
 import 'package:hotelino/model/home/HotelModel.dart';
+import 'package:provider/provider.dart';
 
 class HotelCard extends StatelessWidget {
 
@@ -10,6 +12,41 @@ class HotelCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    return const Placeholder();
+    final favoriteProvider = Provider.of<FavoriteItemProvider>(context);
+    final isFavorite = favoriteProvider.isFavorite(hotel.id as int); /////////////////////////cast
+
+
+    return SizedBox(
+      width: 280,
+      child: Card(
+        elevation: 4,
+        margin: EdgeInsets.all(0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+              Stack(
+
+              ),
+            Expanded(
+                child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                SizedBox(height: 8),
+                Row(
+                  children: [
+                    SizedBox(width: 8),
+                    Icon(Icons.start, color: Colors.amber, size: 20),
+                    SizedBox(width: 4),
+                    Text('${hotel.rating} (${hotel.reviewCount})')
+                  ],
+                )
+              ],
+            ))
+
+
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/features/home/presentation/provider/widgets/hotel_list_section.dart
+++ lib/features/home/presentation/provider/widgets/hotel_list_section.dart
@@ -1,16 +1,18 @@
-
 import 'package:flutter/material.dart';
 import 'package:hotelino/model/home/HotelModel.dart';
-
 import 'hotel_card.dart';
 
-class HotelListSection  extends StatelessWidget {
-
+class HotelListSection extends StatelessWidget {
   final String title;
   final List<HotelModel> hotels;
   final VoidCallback? onSeeAllPressed;
 
-  const HotelListSection ({super.key, required this.title, required this.hotels, this.onSeeAllPressed});
+  const HotelListSection({
+    super.key,
+    required this.title,
+    required this.hotels,
+    this.onSeeAllPressed,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -23,32 +25,35 @@ class HotelListSection  extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               TextButton(
-                  onPressed: () {},
-                  child: Text(
-                    "مشاهده همه",
-                    style: TextStyle(color: Theme.of(context).colorScheme.primary),
-                  )),
-              Text(
-                title,
-                style: Theme.of(context).textTheme.displayMedium,
-              )
+                onPressed: () {},
+                child: Text(
+                  "مشاهده همه",
+                  style: TextStyle(
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                ),
+              ),
+              Text(title, style: Theme.of(context).textTheme.displayMedium),
             ],
           ),
-
         ),
-          SizedBox(
+        SizedBox(
           height: 360,
-         child: ListView.builder(
-       padding: EdgeInsets.only(right: 16),
-        reverse: true,
-       clipBehavior: Clip.none,
-       scrollDirection: Axis.horizontal,
-       itemCount: hotels.length,
-        itemBuilder: (context, index) {
-       return Padding(padding: EdgeInsets.only(left: 16), child: HotelCard(hotel: hotels[index]));
-       },
+          child: ListView.builder(
+            padding: EdgeInsets.only(right: 16),
+            reverse: true,
+            clipBehavior: Clip.none,
+            scrollDirection: Axis.horizontal,
+            itemCount: hotels.length,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: EdgeInsets.only(left: 16),
+                child: HotelCard(hotel: hotels[index]),
+              );
+            },
+          ),
         ),
-          ],
-        );
-     }
+      ],
+    );
+  }
 }
