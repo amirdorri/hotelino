67701d3083b9475626de9dccbfa086b372094585
Amir <amirdorri81@gmail.com>
2025-11-26 11:47:15 +0330

    add booking provider and repo(blank)

---
+++ lib/features/favorite/data/booking_repository.dart
@@ -0,0 +1,10 @@
+
+
+/*
+
+Since this project isn’t real production and there’s no backend yet,
+the repository layer is empty. If we get a backend in the future,
+that’s where we’ll add the code.
+
+
+*/
---
+++ lib/features/favorite/data/favorite_provider.dart
@@ -0,0 +1,35 @@
+
+import 'package:flutter/foundation.dart';
+import 'package:hotelino/model/booking/booking_model.dart';
+import 'package:persian_datetime_picker/persian_datetime_picker.dart';
+
+class FavoriteProvider with ChangeNotifier {
+  final BookingModel _booking = BookingModel();
+
+  BookingModel get booking => _booking;
+
+  void setName(String newName) {
+    _booking.fullName = newName;
+    notifyListeners();
+  }
+
+  void setDestination(String value) {
+    _booking.destination = value;
+    notifyListeners();
+  }
+
+  void setPhoneNumber(String value) {
+    _booking.phoneNumber = value;
+    notifyListeners();
+  }
+
+  void setNumberOfGuests(String value) {
+    _booking.numberOfGuests = value;
+    notifyListeners();
+  }
+
+  void setDateRange(JalaliRange value) {
+    _booking.checkInOutRangeDate = value;
+    notifyListeners();
+  }
+}
