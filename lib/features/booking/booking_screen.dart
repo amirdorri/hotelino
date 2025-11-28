f11f8968e8a503bbf33aa0c83dc261f3d874a44b
Amir <amirdorri81@gmail.com>
2025-11-28 19:51:38 +0330

    booking_form_field.dart used in booking screen

--- lib/features/booking/booking_screen.dart
+++ lib/features/booking/booking_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hotelino/features/booking/booking_provider.dart';
+import 'package:hotelino/features/booking/presentation/booking_form_field.dart';
 import 'package:provider/provider.dart';
 
 class BookingScreen extends StatefulWidget {
@@ -33,12 +34,67 @@ class _BookingScreenState extends State<BookingScreen> {
         padding: const EdgeInsets.all(16),
         child: SingleChildScrollView(
           child: Consumer<BookingProvider>(
-            builder: (context, value, child) {
+            builder: (context, bookingProvider, child) {
               return Form(
                 key: _formKey,
                 child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
+                      BookingFormField(
+                        title: 'نام و نام خانوادگی',
+                        hint: 'نام و نام خانوادگی خود را وارد کنید...',
+                        keyboardType: TextInputType.text,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'لطفا نام خود را کامل بنویسید';
+                          }
+                          return null;
+                        },
+                        initialValue: bookingProvider.booking.fullName,
+                        onSaved: (newValue) {
+                          if (newValue != null) {
+                            bookingProvider.setName(newValue);
+                          }
+                        },
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      BookingFormField(
+                        title: 'تعداد نفرات',
+                        hint: 'تعداد نفرات خود را وارد کنید...',
+                        keyboardType: TextInputType.number,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'لطفا تعداد نفرات را مشخص کنید';
+                          }
+
+                          return null;
+                        },
+                        initialValue: bookingProvider.booking.numberOfGuests,
+                        onSaved: (newValue) {
+                          if (newValue != null) {
+                            bookingProvider.setNumberOfGuests(newValue);
+                          }
+                        },
+                      ),
+                      SizedBox(height: 8),
+                      // NumberFormField(
+                      //   initialValue: bookingProvider.booking.phoneNumber,
+                      //   validator: (value) {
+                      //     if (value == null || value.isEmpty) {
+                      //       return 'لطفا شماره را به درستی وارد کنید';
+                      //     }
+                      //     return null;
+                      //   },
+                      //   onSaved: (newValue) {
+                      //     if (newValue != null) {
+                      //       bookingProvider.setPhoneNumber(newValue);
+                      //     }
+                      //   },
+                      // ),
+
+
 
                 ]),
               );
