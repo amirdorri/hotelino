9dc8864c93db91d6631216bce3285c94603eeb13
Amir <amirdorri81@gmail.com>
2025-12-17 09:29:04 +0330

    add CustomScrollView in HotelDetailScreen.dart

--- lib/features/hotel_detail/presentaion/HotelDetailScreen.dart
+++ lib/features/hotel_detail/presentaion/HotelDetailScreen.dart
@@ -1,6 +1,7 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:hotelino/core/utils/network.dart';
 import 'package:hotelino/features/home/data/hotel_repository.dart';
 import 'package:hotelino/model/home/HotelModel.dart';
 import 'package:hotelino/shared/service/json_data_service.dart';
@@ -26,7 +27,25 @@ class HotelDetailScreen extends StatelessWidget {
         }
 
         final hotel = snapshot.data!;
-        return Scaffold();
+        return Scaffold(
+          body: CustomScrollView(
+            slivers: [
+            SliverAppBar(
+              floating: true,
+              pinned: false,
+              flexibleSpace: FlexibleSpaceBar(
+                background: GestureDetector(
+                  onLongPress: (){},
+                  child: Image.network(
+                      fit: BoxFit.cover,
+                      networkUrl(hotel.images.first)
+                  ),
+                ),
+              ),
+            )
+            ],
+          ),
+        );
 
 
       },
